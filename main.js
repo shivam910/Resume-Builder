ee5fa17eee1efb72a6c271ee4b48715af736ca33
shivam910 <shivams91099@gmail.com>
2021-08-05 15:17:57 +0530

    complete

--- main.js
+++ main.js
@@ -1,14 +1,70 @@
-const inputfield = document.querySelector(".input-fields");
-let inputshow = true;
+const btn = document.querySelector("button");
+btn.style.cursor = "pointer";
+btn.style.width = "150px";
+btn.style.color = "white";
 
-function toggle() {
-    if (inputshow == true) {
-        inputfield.style.display = "none";
-        inputshow = false;
-    }
-    else{
+const input = document.querySelector('.input-fields')
+const output = document.querySelector('.output')
+
+let inputShow = true
+
+async function TextEditor(element){
+  const newEditor =  await ClassicEditor
+  .create(element,{
+    toolbar: [ 'heading', 'bold', 'italic', 'bulletedList', 'numberedList', 'blockQuote' ],
+  } )
+  return newEditor
+ 
+}
+
+let workExpdetails;
+TextEditor(input["workexp"]).then(nEditor=>{
+  workExpdetails = nEditor
+})
+let Academic;
+TextEditor(input["academics"]).then(nEditor=>{
+  Academic = nEditor
+})
+
+
+
+function toggle(){
+    if(inputShow){
+         input.style.display = "none"
+         inputShow = false 
+         output.innerHTML=`
+           <div class="hero">
+            <h1>${input["name"].value}</h1>
+            <h3>${input["title"].value}</h3>
+           </div>
+           <div class="main">
+               <div>
+                 <h2>OBJECTIVE</h2>
+                 <p>${input["objective"].value}</p>
+                 <h2>SKILLS</h2>
+                 <p>${input["skills"].value}</p>
+                 <h2>ACHIEVEMENTS</h2>
+                 <p>${input["achievements"].value}</p>
+                 <h2>CONTACT</h2>
+                 <p>${input["contact"].value}</p>
+               </div>
+               <div>
+                <h2>WORK EXPERIENCE</h2>
+                ${workExpdetails.getData()}
+                <h2>ACEMEDIC DETAILS</h2>
+                 ${Academic.getData()}
+                <h2>PROJECTS</h2>
+                <p>${input["projects"].value}</p>
+               </div>
+           </div>
+           <div class="btn">
+              <button onclick="print()">Print Resume</button>
+        </div>
+         `
+    }else{
+        input.style.display =  "block"
+        inputShow = true
+        output.innerHTML=""
     }
 }
 
-const btn = document.querySelector("button");
-btn.style.cursor= "pointer";
